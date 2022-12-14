0fc6e05ef29b62892100320e61f820218a54915a
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-14 00:50:55 -0500

    Reactive forms: switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // this.miFormulario.setValue(this.persona);
+    this.miFormulario.reset({ ...this.persona, condiciones: false });
+    // this.miFormulario.controls['condiciones'].valueChanges.subscribe(newValue => {console.log(newValue);})
+    this.miFormulario.valueChanges.subscribe(({condiciones, ...rest}) => {
+      // delete form.condiciones;
+      this.persona = rest;
+    });
+  }
+
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    // console.log(formValue);
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
 }
