4fd92d4ce9198b21da1ab2153399d1d248c3a8f9
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-14 12:05:13 -0500

    Validaciones, expresiones regualares

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
 
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [
+      '',
+      [Validators.required, Validators.pattern(nombreApellidoPattern)],
+    ],
+    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
+    username: ['', [Validators.required, noPuedeSerStrider]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'Fabiaan Montoya',
+      email: 'prueba1@web.com',
+      username: 'f4biaan',
+    });
+  }
+
+  campoNoValido(campo: string) {
+    return (
+      this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+    );
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
+  }
 }
